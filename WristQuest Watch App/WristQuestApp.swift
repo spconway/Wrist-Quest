6e409a366737424921338e00d57153cc2139733c
Stephen Conway <stephen.conway87@yahoo.com>
2025-06-13 21:45:55 -0400

    Initial Commit

---
+++ WristQuest Watch App/WristQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristQuestApp.swift
+//  WristQuest Watch App
+//
+//  Created by Stephen Conway on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WristQuest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
